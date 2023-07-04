836815effb1a7637bc98c941116b6d2cd0adbf6f
NehaS101 <solankineha297@gmail.com>
2023-07-04 21:46:45 +0530

    second problem done

--- Set 1/dataType.py
+++ Set 1/dataType.py
@@ -7,3 +7,31 @@
 #integer 
 age=5
 print(f"{type(age)}, value: {age}" )
+
+#float
+temp=25.8
+print(f"{type(temp)}, value: {temp}")
+
+#string
+name="neha solanki"
+print(f"{type(name)}, value: {name}")
+
+#boolean
+is_hot=True
+print(f"{type(is_hot)}, value: {is_hot}")
+
+#list
+nums = [1,2,3,4,5,6,7]
+print(f"{type(nums)}, values: {nums}")
+
+#dictionary
+obj = {"name":"neha","age":5}
+print(f"{type(obj)}, value: {obj}")
+
+#tuple
+tuples = (0,1,2,3)
+print(f"{type(tuples)}, value: {tuples}")
+
+#sets
+set={2,4,5,8,9}
+print(f"{type(set)}, value: {set}")
