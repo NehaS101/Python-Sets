cef7b8841fc691bf81fb7e6b063fc17ec7dc7b2a
NehaS101 <solankineha297@gmail.com>
2023-07-05 01:09:39 +0530

    tenth problem done

--- Set 1/fibonacci.py
+++ Set 1/fibonacci.py
@@ -3,3 +3,18 @@
     *Input*: 5
     *Output*: "[0, 1, 1, 2, 3]"
 '''
+def fibonacci(num):
+ fibon =[0,1]
+
+ if num <= 2:
+  return fibon[:num]
+ 
+ while len(fibon) < num:
+  next = fibon[-1]+fibon[-2]
+  fibon.append(next)
+
+  return fibon
+ 
+num=4
+res=fibonacci(num)
+print(res) 
