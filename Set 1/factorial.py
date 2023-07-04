f5ab8191a673e631b074c871376a5e0289dde8fa
NehaS101 <solankineha297@gmail.com>
2023-07-05 01:04:12 +0530

    ninth problem done

--- Set 1/factorial.py
+++ Set 1/factorial.py
@@ -3,3 +3,15 @@
     *Input*: 5
     *Output*: "Factorial of 5 is 120."
 '''
+
+
+def fact(num):
+    fac=1
+    for i in range(1,num+1):
+        fac = fac*i
+
+    return fac
+
+num=5
+res=fact(num)
+print(f"Factorial of {num} is {res}")
