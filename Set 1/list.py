b841f145c538caf72fe64ffc97843889f94bf2b1
NehaS101 <solankineha297@gmail.com>
2023-07-04 23:27:21 +0530

    fourth problem done

--- Set 1/list.py
+++ Set 1/list.py
@@ -3,3 +3,19 @@
     *Input*: None
     *Output*: "[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20]..."
 '''
+
+#create a list of numbers
+nums=list(range(1,10+1))
+print(nums)
+
+#adding no
+nums.append(9)
+print(nums)
+
+#removing a no
+nums.remove(1)
+print(nums)
+
+#sort the list
+nums.sort()
+print(nums)
