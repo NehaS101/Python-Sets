efffba441e002ec2d6b234338a038956ecfc22c6
NehaS101 <solankineha297@gmail.com>
2023-07-05 01:02:27 +0530

    eight problem done

--- Set 1/prime.py
+++ Set 1/prime.py
@@ -3,3 +3,20 @@
     *Input*: 13
     *Output*: "13 is a prime number."
 '''
+
+import math
+def check(num):
+    if num<2:
+        return False
+    prime=True
+    for i in range(2,int(math.sqrt(num))+1) :
+        if num%i==0:
+            prime=False
+            break
+    return prime
+num=13
+output=check(num)
+if output==True:
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is a prime number")
