180161936e36499ce6c56edaf265951d9060d0eb
NehaS101 <solankineha297@gmail.com>
2023-07-04 23:42:34 +0530

    fifth problem done

--- Set 1/sum&avg.py
+++ Set 1/sum&avg.py
@@ -3,3 +3,14 @@
     *Input*: [10, 20, 30, 40]
     *Output*: "Sum: 100, Average: 25.0"
 '''
+
+nums = input("enter no's :")
+
+# Split the input string into a list of strings
+number = nums.split()
+# Convert the strings into integers
+numbers = [int(x) for x in number]
+
+sums = sum(numbers)
+avg = sums/len(numbers)
+print(f"Sum: {sums}, Average: {avg}")
