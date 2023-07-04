80dcd034313198c1125631e19650dec4375448ef
NehaS101 <solankineha297@gmail.com>
2023-07-05 00:59:07 +0530

    seventh problem done

--- Set 1/vowels.py
+++ Set 1/vowels.py
@@ -3,3 +3,15 @@
     *Input*: "Hello"
     *Output*: "Number of vowels: 2"
 '''
+
+def vowel(string):
+    vowels="aeiouAEIOU"
+    count=0
+    for key in string:
+        if key in vowels:
+            count+=1
+    return count
+
+string="Hello"
+result=vowel(string)
+print(result)
