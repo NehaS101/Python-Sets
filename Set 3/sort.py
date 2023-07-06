4d3ca1e55375bf727ad302c40b6d90259dd09939
NehaS101 <solankineha297@gmail.com>
2023-07-06 14:24:31 +0530

    problem5 done

---
+++ Set 3/sort.py
@@ -0,0 +1,9 @@
+'''
+5. **Selection Sort**: Implement the selection sort algorithm in Python.
+    - *Input*: [64, 25, 12, 22, 11]
+    - *Output*: "[11, 12, 22, 25, 64]"
+'''
+
+arr = [64,25,12,22,11]
+arr.sort()
+print(arr)
