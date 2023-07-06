d43c7abd37eb8beb924a9bb999be818f49226315
NehaS101 <solankineha297@gmail.com>
2023-07-06 12:59:14 +0530

    problem10 done

---
+++ Set 2/modifyTuple.py
@@ -0,0 +1,20 @@
+'''
+Problem10: Modify the tuple
+
+Given a nested tuple. Write a program to modify the first item (22) of a list inside the following tuple to 222
+
+**Given**:
+tuple1 = (11, [22, 33], 44, 55)
+
+**Expected output:**
+tuple1: (11, [222, 33], 44, 55)
+
+'''
+
+tuple1 = (11, [22, 33], 44, 55)
+
+list1=list(tuple1)
+list1[1][0]=222
+tuple1=tuple(list1)
+
+print(tuple1)
